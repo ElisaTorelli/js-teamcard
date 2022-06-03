0d98e403fdff441241919a05bec4a87eadb98009
ElisaTorelli <elisa.torelli1@gmail.com>
2022-06-03 14:11:53 +0200

    project setup

---
+++ main.js
@@ -0,0 +1,32 @@
+const teams = [
+  {
+    img: "wayne-barnett-founder-ceo.jpg",
+    name: "Wayne Barnet",
+    role: "Founder & CEO",
+  },
+  {
+    img: "angela-caroll-chief-editor.jpg",
+    name: "Angela Caroll",
+    role: "Chief Editor",
+  },
+  {
+    img: "walter-gordon-office-manager.jpg",
+    name: "Walter Gordon",
+    role: "Office Manager",
+  },
+  {
+    img: "angela-lopez-social-media-manager.jpg",
+    name: "Angela Lopez",
+    role: "Social Media Manager",
+  },
+  {
+    img: "scott-estrada-developer.jpg",
+    name: "Scott Estrada",
+    role: "Developer",
+  },
+  {
+    img: "barbara-ramos-graphic-designer.jpg",
+    name: "Barbara Ramos",
+    role: "Graphic Designer",
+  },
+];
